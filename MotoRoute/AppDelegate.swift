9a8adfa880cac20ee60f72c905ccee6eb64d2ed3
Raphael Bolliger <raphi@raphibolliger.ch>
2015-03-01 12:19:54 +0100

    Initial Commit

---
+++ MotoRoute/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MotoRoute
+//
+//  Created by Raphael Bolliger on 01.03.15.
+//  Copyright (c) 2015 Raphael Bolliger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MotoRoute/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MotoRoute
+//
+//  Created by Raphael Bolliger on 01.03.15.
+//  Copyright (c) 2015 Raphael Bolliger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
