3d8db3462d107ba5e466d1bd71dc60342fec160b
Raphael Bolliger <raphi@raphibolliger.ch>
2015-03-03 14:10:51 +0100

    added route calculation

--- MotoRoute/ViewController.swift
+++ MotoRoute/ViewController.swift
@@ -10,16 +10,17 @@ import Cocoa
 import CoreLocation
 import MapKit
 
-class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate, MKMapViewDelegate {
 
     @IBOutlet var mapSwitcher: NSSegmentedControl!
     @IBOutlet weak var locationMap: MKMapView!
     @IBOutlet weak var wayPointTable: NSTableView!
     
+    var placemarkList: [CLPlacemark] = []
     var wayPointList: [WayPoint] = []
     var showMyLocationFlag: Bool = false;
     
-    var geoCoder = CLGeocoder()
+    var geoLocation = CLGeocoder()
     
     let nib = NSNib(nibNamed: "TableViewCell", bundle: NSBundle.mainBundle())
     
@@ -27,6 +28,8 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         locationMap.showsUserLocation = showMyLocationFlag
+        locationMap.delegate = self
+        
         self.wayPointTable.registerNib(nib!, forIdentifier: "TableViewCell")
         // Do any additional setup after loading the view.
         
@@ -45,18 +48,6 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     
     @IBAction func switchMap(sender: AnyObject) {
         
-        
-    }
-    
-    
-    @IBAction func didClickOnTableView(sender: AnyObject) {
-        
-        if (wayPointList.count <= 0) {
-            return
-        }
-        
-        locationMap.selectAnnotation(wayPointList[wayPointTable.selectedRow].annotation, animated: true)
-        
     }
     
     private func showActualAnnotation(){
@@ -67,21 +58,100 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         
         var gestureRecognizer: NSGestureRecognizer = sender as NSGestureRecognizer
         let mapViewCord = gestureRecognizer.locationInView(locationMap)
+        
         var locCord = locationMap.convertPoint(mapViewCord, toCoordinateFromView: locationMap!)
         
-        var annotation = MKPointAnnotation()
-        annotation.setCoordinate(locCord)
-        annotation.title = "Wegpunkt: \(wayPointList.count + 1) \(annotation.coordinate.latitude) \(annotation.coordinate.longitude)"
+        loadLocality(locCord.latitude, longitude: locCord.longitude)
         
-        wayPointList.append(WayPoint(latitude: locCord.latitude, longitude: locCord.longitude, annotation: annotation))
-        locationMap.addAnnotation(annotation)
         
+    }
+    
+    func loadLocality(latitude: Double, longitude: Double) {
+        
+        var placemark : CLPlacemark!
+        var location = CLLocation(latitude: latitude, longitude: longitude)
+        
+        geoLocation.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+            if (error != nil) {
+                println("Reverse geocode failed with error")
+            }
+            if placemarks.count > 0 {
+                placemark = placemarks[0] as CLPlacemark
+                println(placemark.locality)
+                
+                self.placemarkList.append(placemark)
+                
+                var annotation = MKPointAnnotation()
+                annotation.setCoordinate(CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+                annotation.title = placemark.locality
+                annotation.subtitle = "\(annotation.coordinate.latitude) \(annotation.coordinate.longitude)"
+                
+                var wayPointNew = WayPoint(locality: placemark.locality ,latitude: latitude, longitude: longitude, annotation: annotation)
+                
+                self.wayPointList.append(wayPointNew)
+                self.locationMap.addAnnotation(annotation)
+                
+                self.wayPointTable.reloadData()
+                
+            } else {
+                println("Problem with the date recieved from geocoder")
+            }
+        })
+        
+    }
+    
+    @IBAction func deleteWayPoint(sender: AnyObject) {
+        
+        if wayPointList.count <= 0 { return }
+        locationMap.removeAnnotation(wayPointList[wayPointTable.selectedRow].annotation)
+        wayPointList.removeAtIndex(wayPointTable.selectedRow)
         wayPointTable.reloadData()
         
     }
     
+    @IBAction func calculateRoute(sender: AnyObject) {
+        
+        for var index = 0; index < placemarkList.count-1; index++ {
+            
+            var startPoint = MKMapItem(placemark: MKPlacemark(placemark: placemarkList[index]))
+            var endPoint = MKMapItem(placemark: MKPlacemark(placemark: placemarkList[index+1]))
+            
+            let request = MKDirectionsRequest()
+            request.setSource(startPoint)
+            request.setDestination(endPoint)
+            request.requestsAlternateRoutes = true
+            
+            let directions = MKDirections(request: request)
+            
+            directions.calculateDirectionsWithCompletionHandler({
+                (response: MKDirectionsResponse!, error: NSError!) in
+                
+                if error != nil {
+                    // Handle error
+                } else {
+                    self.locationMap.addOverlay(response.routes[0].polyline, level: MKOverlayLevel.AboveRoads)
+                    println("\(response.destination.description)")
+                }
+                
+            })
+            
+        }
+        
+    }
+    
+    
     func numberOfRowsInTableView(tableView: NSTableView) -> Int {
         return wayPointList.count
+    }
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        
+        if (wayPointList.count <= 0) {
+            return
+        }
+        
+        locationMap.selectAnnotation(wayPointList[wayPointTable.selectedRow].annotation, animated: true)
+        
     }
     
     
@@ -89,10 +159,16 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         let view = wayPointTable.makeViewWithIdentifier("TableViewCell", owner: self) as TableViewCell
         view.title.stringValue = wayPointList[row].locality
         view.subTitle.stringValue = wayPointList[row].locationToString()
-        view.numPoint.stringValue = "\(row)"
         return view
     }
     
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        
+        renderer.strokeColor = NSColor.blueColor()
+        renderer.lineWidth = 5.0
+        return renderer
+    }
 
 }
 
--- MotoRoute/WayPoint.swift
+++ MotoRoute/WayPoint.swift
@@ -8,44 +8,21 @@
 
 import Cocoa
 import MapKit
-import CoreLocation
 
 class WayPoint {
     
+    var locality: String
     var latitude: Double
     var longitude: Double
     var annotation: MKPointAnnotation
-    var geoLocation = CLGeocoder()
-    var locality: String = "Fuu"
     
-    init(latitude: Double, longitude: Double, annotation: MKPointAnnotation) {
+    init(locality: String, latitude: Double, longitude: Double, annotation: MKPointAnnotation) {
         
+        self.locality = locality
         self.latitude = latitude
         self.longitude = longitude
         self.annotation = annotation
         
-        var placemark : CLPlacemark!
-        
-        let location = CLLocation(latitude: latitude, longitude: longitude)
-        
-        geoLocation.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
-            if (error != nil) {
-                println("Reverse geocode failed with error")
-            }
-            if placemarks.count > 0 {
-                placemark = placemarks[0] as CLPlacemark
-                self.setLocality(placemark.locality)
-            } else {
-                println("Problem with the date recieved from geocoder")
-            }
-        })
-        
-        22
-        
-    }
-    
-    func setLocality(l: String) {
-        locality = l
     }
     
     func getLocation() -> CLLocationCoordinate2D {
@@ -53,9 +30,7 @@ class WayPoint {
     }
     
     func locationToString() -> String {
-        
         return "\(latitude)  \(longitude)"
-        
     }
 
 }
